4f89ab25300fb95a4d940ae000584ceb912b349a
Will Rogers <wahrogers@gmail.com>
2017-04-19 15:08:26 +0100

    Add a couple more tests.

--- test_cube.py
+++ test_cube.py
@@ -2,6 +2,7 @@ import numpy
 import cube
 
 
+ONE = numpy.array([[0, 0, 0]]).T
 TWO = numpy.array([[0, 0, 0], [0, 0, 1]]).T
 
 
@@ -16,12 +17,23 @@ def test_locations():
     assert len(locations) == 48
 
 
-def test_orientations():
+def test_orientations_for_1x1_cube():
+    orientations = cube.orientations(ONE)
+    assert len(orientations) == 1
+    numpy.testing.assert_equal(orientations[0], ONE)
+
+
+def test_orientations_for_2x1():
     orientations = cube.orientations(TWO)
-    assert len(orientations)
+    assert len(orientations) == 3
     numpy.testing.assert_equal(orientations[0],
                                numpy.array([[0, 0, 0], [0, 0, 1]]).T)
     numpy.testing.assert_equal(orientations[1],
                                numpy.array([[0, 0, 0], [0, 1, 0]]).T)
     numpy.testing.assert_equal(orientations[2],
                                numpy.array([[0, 0, 0], [1, 0, 0]]).T)
+
+
+def test_orientations_for_R_CROSS():
+    orientations = cube.orientations(cube.R_CROSS)
+    assert len(orientations) == 3
